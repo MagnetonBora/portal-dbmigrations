61e127af4c135a4f50e2e33dd5786416f856c5cf
Roman Dryndik <dryndikroman@gmail.com>
2020-03-01 16:50:19 +0100

    Initial commit

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+URI = 'mysql+pymysql://roman:secret@127.0.0.1:3306/portal'
+
+engine = create_engine(URI)
+Base = declarative_base(bind=engine)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    nickname = Column(String(50))
+
